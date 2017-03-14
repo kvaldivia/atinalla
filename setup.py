155db24a1e579ad75cf5ec5f410a998d969e1fe4
Kenny Valdivia <kvaldivia@yandex.com>
2017-03-13 23:55:53 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'atinalla',
+  packages = ['atinalla'],
+  version = '0.1',
+  description = 'A reportlab wrapper',
+  author = 'Kenny Valdivia',
+  author_email = 'kvaldivia@yandex.com',
+  url = 'https://github.com/kvaldivia/atinalla',
+  download_url = 'https://github.com/kvaldivia/atinalla',
+  keywords = ['reportlab', 'wrapper', 'reports', 'pdf', 'odoo'],
+  classifiers = [],
+)
